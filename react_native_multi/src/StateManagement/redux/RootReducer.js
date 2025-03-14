84996aae46f261eafd50f2a75123516dd053edf9
kyungyunie <yky0306129@gmail.com>
2025-03-14 16:31:21 +0900

    Delete react_native_multi directory

--- react_native_multi/src/StateManagement/redux/RootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import todoSlice from './TodoRedux';
-
-const rootReducer = combineReducers({
-    todos: todoSlice,
-});
-
-export default rootReducer;
